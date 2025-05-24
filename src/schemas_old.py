af6abd279fd5865811b91da40c0cfea1ceba7f8c
kremenevskiy <kremeneuski@gmail.com>
2025-05-24 04:28:27 +0300

    rm outdated

--- src/schemas_old.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class ValidateRequest(BaseModel):
-    transactionId: str = Field(..., alias="transactionId")
-    signedTransactionInfo: str | None = None
-    environment: str = "Sandbox"
-    appAccountToken: str
